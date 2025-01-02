79adb946d5a6517e06e333dab5ea77630f84abd0
rohitranjanab1718 <rohitranjanab1718@gmail.com>
2025-01-03 00:25:07 +0530

    heroku push

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 
@@ -19,6 +20,20 @@ const postRouter = require("./routes/post");
 
 app.use("/api", authRouter);
 app.use("/api",postRouter);
+
+const parentDirectory = path.resolve(__dirname, "..","..");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(parentDirectory, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(parentDirectory, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 connectDB()
   .then(() => {
